c55756c78a6fd07ba7c369c0393e37b98878aba8
Jerome Etienne <jerome.etienne@gmail.com>
2010-07-27 12:24:20 +0200

    hostname+port in cmdline, count the client's connection 	modified:   node-chargen.js

--- node-chargen.js
+++ node-chargen.js
@@ -2,58 +2,96 @@
 
 var http	= require('http'); 
 
-var create	= function(opts){
+var create	= function(ctor_opts){
+	//////////////////////////////////////////////////////////////////////////
+	//		class variables						//
+	//////////////////////////////////////////////////////////////////////////	
 	// alias opts for readability and default values
-	var verbose	= opts.verbose 		|| 0;
-	var loop_delay	= opts.loop_delay	|| 5;
+	var verbose	= ctor_opts.verbose 	|| 0;
+	var loop_delay	= ctor_opts.loop_delay	|| 5;
+	var hostname	= ctor_opts.hostname	|| '127.0.0.1';
+	var port	= ctor_opts.port	|| 8124;
+
+	//////////////////////////////////////////////////////////////////////////
+	//		ctor/dtor						//
+	//////////////////////////////////////////////////////////////////////////
+	var ctor	= function(){
+		server_start();
+	}
+	var dtor	= function(){
+		//console.log("Stop neoip rpc call");
+		server_stop();
+	}
+	
+	//////////////////////////////////////////////////////////////////////////
+	//		server							//
+	//////////////////////////////////////////////////////////////////////////
+	var server	= null;
+	var nb_cnx	= 0;
 	// pattern which gonna be displayed
 	var pattern	= "!\"#$%&'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\]^_`abcdefg";
-	// create the http server
-	var server	= http.createServer(function (request, response) {
-		if( verbose > 0 )	console.log("Client connected");
-		response.writeHead(200, {
-			'Content-Type'		: 'text/plain',
-			'Transfer-Encoding'	: 'identity'		// to force a non-chunked response
-		});
-		var nb_line	= 0;
-		var stoploop	= false;
-		// to findout when the client closes the connection
-		request.connection.addListener('error', function(){
-			stoploop	= true;
-		});
-		var response_stop	= function(){
-			if( verbose > 0 )	console.log("Client disconnected");
-			response.end();			
-		}
-		/**
-		 * loop and deliver the usual chargen pattern until the end
-		*/
-		var main_loop_get	= function(){
-			// if the client closes the connection, stop looping
-			if(stoploop){
-				response_stop();
-				return;
+	var server_start	= function(){
+		// create the http server
+		server	= http.createServer(function (request, response) {
+			// update nb_cnx: add this connection
+			nb_cnx		+= 1;
+			if( verbose > 0 )	console.log("Client connected ("+nb_cnx+' now)');
+			response.writeHead(200, {
+				'Content-Type'		: 'text/plain',
+				'Transfer-Encoding'	: 'identity'		// to force a non-chunked response
+			});
+			var nb_line	= 0;
+			var stoploop	= false;
+			// to findout when the client closes the connection
+			request.connection.addListener('error', function(){
+				stoploop	= true;
+			});
+			var response_stop	= function(){
+				// update nb_cnx: remove this connection
+				nb_cnx		-= 1;
+				if( verbose > 0 )	console.log("Client disconnected ("+nb_cnx+' now)');
+				response.end();			
 			}
-			// compute the content to send
-			var offset	= nb_line % pattern.length;
-			var content	= pattern.substr(offset, pattern.length-offset) + pattern.substr(0, offset) + '\n';
-			nb_line++;
-			// write the content to the response
-			response.write(content);
-			// log to debug
-			if( verbose > 1 )	console.log("One line written");
-			// defer the next iteration 
-			setTimeout(main_loop_get, loop_delay);
-		}
-		// start looping
-		if( request.method == "GET" ){
-			main_loop_get();
-		}else if( request.method == "HEAD" ){
-			response_stop();
-		}
-	});
-	// return the just-created server
-	return server;
+			/**
+			 * loop and deliver the usual chargen pattern until the end
+			*/
+			var main_loop_get	= function(){
+				// if the client closes the connection, stop looping
+				if(stoploop){
+					response_stop();
+					return;
+				}
+				// compute the content to send
+				var offset	= nb_line % pattern.length;
+				var content	= pattern.substr(offset, pattern.length-offset) + pattern.substr(0, offset) + '\n';
+				nb_line++;
+				// write the content to the response
+				response.write(content);
+				// log to debug
+				if( verbose > 1 )	console.log("One line written");
+				// defer the next iteration 
+				setTimeout(main_loop_get, loop_delay);
+			}
+			// start looping
+			if( request.method == "GET" ){
+				main_loop_get();
+			}else if( request.method == "HEAD" ){
+				response_stop();
+			}
+		});
+		server.listen(port, hostname);
+	}
+
+	//////////////////////////////////////////////////////////////////////////
+	//		run initialisation					//
+	//////////////////////////////////////////////////////////////////////////
+	// call the contructor
+	ctor();
+	// return the public properties
+	return {
+		destroy		: dtor,
+		client_url	: function(){ return "http://"+hostname+":"+port+"/"}
+	}
 }
 
 // export it via commonjs
@@ -64,29 +102,41 @@ exports.create	= create;
 //		Main code							//
 //////////////////////////////////////////////////////////////////////////////////
 if( process.argv[1] == __filename ){
-	var verbose	= 0;
-	var loop_delay	= null;
-
 	//////////////////////////////////////////////////////////////////////////////////
 	//	parse cmdline								//
 	//////////////////////////////////////////////////////////////////////////////////
+	// cmdline_opts default
+	cmdline_opts	= {
+		verbose		: 0,
+		loop_delay	: null,
+		hostname	: null,
+		port		: null
+	};
 	var optind	= 2;
 	for(;optind < process.argv.length; optind++){
 		var key	= process.argv[optind];
 		var val	= process.argv[optind+1];
 		//console.log("key="+key+" val="+val);
-		if( key == "-v" || key == "--verbose" ){
-			verbose		+= 1;
-		}else if( key == "-l" || key == "--loop_delay" ){
-			loop_delay	= parseInt(val);
+		if( key == "-a" || key == "--hostname" ){
+			cmdline_opts.hostname	= val;
 			optind		+= 1;
+		}else if( key == "-p" || key == "--port" ){
+			cmdline_opts.port	= parseInt(val);
+			optind		+= 1;
+		}else if( key == "-l" || key == "--loop_delay" ){
+			cmdline_opts.loop_delay	= parseInt(val);
+			optind		+= 1;
+		}else if( key == "-v" || key == "--verbose" ){
+			cmdline_opts.verbose		+= 1;
 		}else if( key == "-h" || key == "--help" ){
-			console.log("usage: node-chargen [-v] [-l n_msec]");
+			console.log("usage: node-chargen [-v] [-l n_msec] [-a hostname] [-p port]");
 			console.log("");
 			console.log("Provide a chargen-like on top of a webserver.");
 			console.log("");
+			console.log("-a|--hostname host\tSet the address to listen on.");
+			console.log("-p|--hostname port\tSet the port to listen on.");
+			console.log("-l|--loop_delay msec\tSet the delay between each line (default to 5-msec).");
 			console.log("-v|--verbose\tIncrease the verbose level (for debug).");
-			console.log("-l|--loop_delay\tSet the delay between each line (default to 5-msec).");
 			console.log("-h|--help\tDisplay the inline help.");
 			process.exit(0);
 		}else{
@@ -95,11 +145,13 @@ if( process.argv[1] == __filename ){
 		}
 	}
 
-	opts	= {
-		"verbose"	: verbose,
-		"loop_delay"	: loop_delay
-	}
-	server	= create(opts);
-	server.listen(8124, "127.0.0.1");
-	console.log('Server running at http://127.0.0.1:8124/');
+	// build ctor_opts
+	ctor_opts	= {}
+	ctor_opts.hostname	= cmdline_opts.hostname		|| null;
+	ctor_opts.port		= cmdline_opts.port		|| null;
+	ctor_opts.loop_delay	= cmdline_opts.loop_delay	|| null;
+	ctor_opts.verbose	= cmdline_opts.verbose		|| null;
+	// create chargen
+	chargen	= create(ctor_opts);
+	console.log('Server running at '+chargen.client_url());
 }
